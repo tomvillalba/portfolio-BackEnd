4e9b72dd5bab6512cba62a107a555db02df65372
Tomas Villalba <totovillalba.tv@gmail.com>
2023-07-04 01:24:10 -0300

    Edit educacion

--- src/main/java/com/example/demo/controllers/EducacionController.java
+++ src/main/java/com/example/demo/controllers/EducacionController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import com.example.demo.models.EducacionModel;
 import com.example.demo.services.EducacionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -25,13 +27,30 @@ public class EducacionController {
     }
 
     @DeleteMapping(path = "/{id}")
-    public String eliminarPorId(@PathVariable("id") Integer id) {
+    public ResponseEntity<RespuestaEliminar> eliminarPorId(@PathVariable("id") Integer id) {
         boolean ok = this.educacionService.eliminarEducacion(id);
         if (ok) {
-            return "Se eliminó la educación con id " + id;
+            RespuestaEliminar respuesta = new RespuestaEliminar("Se eliminó la educación con id " + id);
+            return ResponseEntity.ok(respuesta);
         } else {
-            return "No pudo eliminar la educación con id " + id;
+            RespuestaEliminar respuesta = new RespuestaEliminar("No se pudo eliminar la educación con id " + id);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(respuesta);
         }
     }
 
+}
+class RespuestaEliminar {
+    private String mensaje;
+
+    public RespuestaEliminar(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
 }
