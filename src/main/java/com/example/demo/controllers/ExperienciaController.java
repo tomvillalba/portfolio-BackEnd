6ec700a62add2d53008675dbb8e937e6252fc624
Tomas Villalba <totovillalba.tv@gmail.com>
2023-07-09 19:47:26 -0300

    Deploy en fly.io

--- src/main/java/com/example/demo/controllers/ExperienciaController.java
+++ src/main/java/com/example/demo/controllers/ExperienciaController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import com.example.demo.models.ExperienciaModel;
 import com.example.demo.services.ExperienciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -25,13 +27,16 @@ public class ExperienciaController {
     }
 
     @DeleteMapping(path = "/{id}")
-    public String eliminarPorId(@PathVariable("id") Integer id) {
+    public ResponseEntity<RespuestaEliminar> eliminarPorId(@PathVariable("id") Integer id) {
         boolean ok = this.experienciaService.eliminarExperiencia(id);
         if (ok) {
-            return "Se eliminó la experiencia con id " + id;
+            RespuestaEliminar respuesta = new RespuestaEliminar("Se eliminó la experiencia con id " + id);
+            return ResponseEntity.ok(respuesta);
         } else {
-            return "No pudo eliminar la experiencia con id " + id;
+            RespuestaEliminar respuesta = new RespuestaEliminar("No se pudo eliminar la experiencia con id " + id);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(respuesta);
         }
     }
 
 }
+
